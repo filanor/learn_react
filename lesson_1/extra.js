9c50cfaf2a8dfaf08edb7e5f6a8123fb59b19de8
filanor <filanor@MacBook-Pro-filanor.local>
2019-11-29 15:26:59 +0300

    урок 1 доп

--- lesson_1/extra.js
+++ lesson_1/extra.js
@@ -1,5 +1,32 @@
 let images = [
-	'img/img_1.jpg',
-    'img/img_2.jpg', 
-    'img/img_3.jpg',
+    'https://images.wallpaperscraft.ru/image/zdanie_neon_vyveski_152012_3840x2160.jpg',
+	'img/1.jpg',
+    'img/2.jpg', 
+    'img/3.jpg',
+    'img/4.jpg',
+    'https://i.ytimg.com/vi/M7z5yMWmvx0/maxresdefault.jpg'
 ]
+
+const checkImage = path =>
+    new Promise(resolve => {
+        const img = new Image();
+        img.onload = () => resolve(img);
+        img.onerror = () => resolve({path, status: 'error'});
+
+        img.src = path;
+    });
+
+
+const showImg = (img) => {
+    const container = document.getElementById('container');
+    Promise.all( images.map( checkImage ) )
+        .then(res => {
+            for(let i = 0; i < res.length; i++){
+                container.append(res[i])
+            }
+        })
+        .catch(err=>console.log(err))
+
+}
+
+showImg(images);
